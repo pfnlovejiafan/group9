040b37053ccbcdb5e4bc738d841e274e72e71712
梦辰 <jiafanlovepfn>
2020-12-18 20:54:10 +0800

    删除

--- day04/day042/src/main/java/com/example/day042/mvp/Cellet.java
+++
@@ -1,6 +0,0 @@
-package com.example.day042.mvp;
-
-public interface Cellet {
-    void showOk(Object object);
-    void showNo(String string);
-}
--- day04/day042/src/main/java/com/example/day042/mvp/IView.java
+++
@@ -1,6 +0,0 @@
-package com.example.day042.mvp;
-
-public interface IView {
-    void showOk(Object object);
-    void showNo(String string);
-}
