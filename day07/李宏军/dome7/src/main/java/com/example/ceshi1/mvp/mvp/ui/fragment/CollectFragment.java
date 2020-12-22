82923793b32bf70490aad103d8ae74fa73f43881
与你 <3341138934@qq.com>
2020-12-22 21:18:01 +0800

    李宏军

---
+++ day07/李宏军/dome7/src/main/java/com/example/ceshi1/mvp/mvp/ui/fragment/CollectFragment.java
@@ -0,0 +1,22 @@
+package com.example.ceshi1.mvp.mvp.ui.fragment;
+
+
+import com.example.ceshi1.mvp.base.BaseFragment;
+import com.example.ceshi1.mvp.base.BasePresenter;
+
+public class CollectFragment extends BaseFragment {
+    @Override
+    protected BasePresenter createPresenter() {
+        return null;
+    }
+
+    @Override
+    protected void init() {
+
+    }
+
+    @Override
+    protected int getLayoutId() {
+        return 0;
+    }
+}
