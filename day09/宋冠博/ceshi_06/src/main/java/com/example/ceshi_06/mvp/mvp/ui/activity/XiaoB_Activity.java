1f202d2d77df0e9adefc7eeb2da021b39f6e8fe3
云中探花手 <32460896231@qq.com>
2020-12-25 07:41:43 +0800

    宋冠博

---
+++ day09/宋冠博/ceshi_06/src/main/java/com/example/ceshi_06/mvp/mvp/ui/activity/XiaoB_Activity.java
@@ -0,0 +1,16 @@
+package com.example.ceshi_06.mvp.mvp.ui.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.ceshi_06.R;
+
+public class XiaoB_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_xiao_b_);
+    }
+}
