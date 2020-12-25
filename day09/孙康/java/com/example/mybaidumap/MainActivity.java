d5acbf7f5e07c4bdfb02eea0e20d48a7c88afa49
SunKang <15372992326m@sina.cn>
2020-12-25 08:16:13 +0800

    孙康

---
+++ day09/孙康/java/com/example/mybaidumap/MainActivity.java
@@ -0,0 +1,290 @@
+package com.example.mybaidumap;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.baidu.location.BDAbstractLocationListener;
+import com.baidu.location.BDLocation;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
+import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.BitmapDescriptor;
+import com.baidu.mapapi.map.MapPoi;
+import com.baidu.mapapi.map.MapStatusUpdate;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
+import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MyLocationConfiguration;
+import com.baidu.mapapi.map.MyLocationData;
+import com.baidu.mapapi.model.LatLng;
+import com.baidu.mapapi.overlayutil.PoiOverlay;
+import com.baidu.mapapi.search.core.PoiInfo;
+import com.baidu.mapapi.search.core.SearchResult;
+import com.baidu.mapapi.search.poi.OnGetPoiSearchResultListener;
+import com.baidu.mapapi.search.poi.PoiCitySearchOption;
+import com.baidu.mapapi.search.poi.PoiDetailResult;
+import com.baidu.mapapi.search.poi.PoiDetailSearchOption;
+import com.baidu.mapapi.search.poi.PoiDetailSearchResult;
+import com.baidu.mapapi.search.poi.PoiIndoorResult;
+import com.baidu.mapapi.search.poi.PoiResult;
+import com.baidu.mapapi.search.poi.PoiSearch;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @BindView(R.id.map_view)
+    MapView mMapView;
+    @BindView(R.id.image_loc)
+    Button imageLoc;
+    private BaiduMap mBaiduMap = null;
+    private Context context;
+
+    //定位相关
+    private double mLatitude;
+    private double mLongtitude;
+
+    //方向传感器
+    private MyOrientationListener mMyOrientationListener;
+    private float mCurrentX;
+    //自定义图标
+    private BitmapDescriptor mIconLocation;
+    private LocationClient mLocationClient;
+    public BDAbstractLocationListener myListener;
+    private LatLng mLastLocationData;
+    private boolean isFirstin = true;
+    private PoiSearch mPoiSearch;
+    private PoiResult mPoiResult;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        this.context = this;
+        //获取地图控件引用
+        mBaiduMap = mMapView.getMap();
+        initMyLocation();
+        button();
+        //导航
+
+        //POI
+        poi();
+
+
+        navigation();
+    }
+
+
+
+    private void navigation() {
+
+    }
+
+    @OnClick({R.id.image_loc})
+    public void onClick(View v) {
+        SDKInitializer.initialize(getApplicationContext());
+        switch (v.getId()) {
+            case R.id.image_loc: {
+                centerToMyLocation(mLatitude, mLongtitude);
+                break;
+            }
+        }
+    }
+
+    //按钮响应
+    private void button() {
+        //按钮
+        Button mbut_Loc = (Button) findViewById(R.id.image_loc);
+        //按钮处理
+        mbut_Loc.setOnClickListener(this);
+    }
+
+    //定位
+    private class MyLocationListener extends BDAbstractLocationListener {
+        @Override
+        public void onReceiveLocation(BDLocation location) {
+            //mapView 销毁后不在处理新接收的位置
+            if (location == null || mMapView == null) {
+                return;
+            }
+            MyLocationData locData = new MyLocationData.Builder()
+                    .accuracy(location.getRadius())
+                    // 此处设置开发者获取到的方向信息，顺时针0-360
+                    .direction(mCurrentX).latitude(location.getLatitude())
+                    .longitude(location.getLongitude()).build();
+            mBaiduMap.setMyLocationData(locData);
+            //设置自定义图标
+            MyLocationConfiguration config = new
+                    MyLocationConfiguration(
+                    MyLocationConfiguration.LocationMode.NORMAL, true, mIconLocation);
+            mBaiduMap.setMyLocationConfiguration(config);
+            //更新经纬度
+            mLatitude = location.getLatitude();
+            mLongtitude = location.getLongitude();
+            //设置起点
+            mLastLocationData = new LatLng(mLatitude, mLongtitude);
+            if (isFirstin) {
+                centerToMyLocation(location.getLatitude(), location.getLongitude());
+
+                if (location.getLocType() == BDLocation.TypeGpsLocation) {
+                    // GPS定位结果
+                    Toast.makeText(context, "定位:" + location.getAddrStr(), Toast.LENGTH_SHORT).show();
+                } else if (location.getLocType() == BDLocation.TypeNetWorkLocation) {
+                    // 网络定位结果
+                    Toast.makeText(context, "定位:" + location.getAddrStr(), Toast.LENGTH_SHORT).show();
+                } else if (location.getLocType() == BDLocation.TypeOffLineLocation) {
+                    // 离线定位结果
+                    Toast.makeText(context, "定位:" + location.getAddrStr(), Toast.LENGTH_SHORT).show();
+                } else if (location.getLocType() == BDLocation.TypeServerError) {
+                    Toast.makeText(context, "定位:服务器错误", Toast.LENGTH_SHORT).show();
+                } else if (location.getLocType() == BDLocation.TypeNetWorkException) {
+                    Toast.makeText(context, "定位:网络错误", Toast.LENGTH_SHORT).show();
+                } else if (location.getLocType() == BDLocation.TypeCriteriaException) {
+                    Toast.makeText(context, "定位:手机模式错误，请检查是否飞行", Toast.LENGTH_SHORT).show();
+                }
+                isFirstin = false;
+            }
+        }
+    }
+
+    private void poi() {
+        mPoiSearch = PoiSearch.newInstance();
+        mPoiSearch.setOnGetPoiSearchResultListener(listener);
+        mPoiSearch.searchInCity(new PoiCitySearchOption()
+                .city("北京") //必填
+                .keyword("美食") //必填
+                .pageNum(10));
+    }
+
+    OnGetPoiSearchResultListener listener = new OnGetPoiSearchResultListener() {
+        @Override
+        public void onGetPoiResult(PoiResult poiResult) {
+            mPoiResult = poiResult;
+            if (poiResult.error == SearchResult.ERRORNO.NO_ERROR) {
+                mBaiduMap.clear();
+
+                //创建PoiOverlay对象
+                PoiOverlay poiOverlay = new PoiOverlay(mBaiduMap);
+
+                //设置Poi检索数据
+                poiOverlay.setData(poiResult);
+
+                //将poiOverlay添加至地图并缩放至合适级别
+                poiOverlay.addToMap();
+                poiOverlay.zoomToSpan();
+            }
+        }
+        @Override
+        public void onGetPoiDetailResult(PoiDetailSearchResult poiDetailSearchResult) {
+//PoiInfo 检索到的第一条信息
+            PoiInfo poi = mPoiResult.getAllPoi().get(0);
+//通过第一条检索信息对应的uid发起详细信息检索
+            mPoiSearch.searchPoiDetail((new PoiDetailSearchOption())
+                    .poiUids(poi.uid)); // uid的集合，最多可以传入10个uid，多个uid之间用英文逗号分隔。
+        }
+        @Override
+        public void onGetPoiIndoorResult(PoiIndoorResult poiIndoorResult) {
+
+        }
+        //废弃
+        @Override
+        public void onGetPoiDetailResult(PoiDetailResult poiDetailResult) {
+
+        }
+    };
+    //初始化定位
+    private void initMyLocation() {
+        //缩放地图
+        MapStatusUpdate msu = MapStatusUpdateFactory.zoomTo(15.0f);
+        mBaiduMap.setMapStatus(msu);
+        //开启定位
+        mBaiduMap.setMyLocationEnabled(true);
+        //声明LocationClient类
+        mLocationClient = new LocationClient(this);
+        //通过LocationClientOption设置LocationClient相关参数
+        LocationClientOption option = new LocationClientOption();
+        option.setOpenGps(true); // 打开gps
+        option.setCoorType("bd09ll"); // 设置坐标类型
+        option.setIsNeedAddress(true);//设置是否需要地址信息
+        option.setScanSpan(100);
+        //设置locationClientOption
+        mLocationClient.setLocOption(option);
+        myListener = new MyLocationListener();
+        //注册监听函数
+        mLocationClient.registerLocationListener(myListener);
+        //初始化图标
+        // mIconLocation = BitmapDescriptorFactory.fromResource(R.drawable.navi_map_gps);
+        initOrientation();
+        //开始定位
+        mLocationClient.start();
+    }
+    //回到定位中心
+    private void centerToMyLocation(double latitude, double longtitude) {
+        mBaiduMap.clear();
+        mLastLocationData = new LatLng(latitude, longtitude);
+        MapStatusUpdate msu = MapStatusUpdateFactory.newLatLng(mLastLocationData);
+        mBaiduMap.animateMapStatus(msu);
+    }
+    //传感器
+    private void initOrientation() {
+        //传感器
+        mMyOrientationListener = new MyOrientationListener(context);
+        mMyOrientationListener.setOnOrientationListener(new MyOrientationListener.OnOrientationListener() {
+            @Override
+            public void onOrientationChanged(float x) {
+                mCurrentX = x;
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        //开启定位
+        mBaiduMap.setMyLocationEnabled(true);
+        if (!mLocationClient.isStarted())
+            mLocationClient.start();
+        //开启方向传感器
+        mMyOrientationListener.start();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mMapView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mMapView.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        //停止定位
+        mBaiduMap.setMyLocationEnabled(false);
+        mLocationClient.stop();
+        //停止方向传感器
+        mMyOrientationListener.stop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mPoiSearch.destroy();
+        mBaiduMap.setMyLocationEnabled(false);
+        mMapView.onDestroy();
+        mMapView = null;
+    }
+}
